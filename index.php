4c5a20ee290554984772fd3d7729e34f43fc2e5c
MartinO55 <cas022103@coderacademy.edu.au>
2022-07-02 13:03:41 +1000

    can now summon and iterate through the JSOn data with some degree of confidence

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+    $fileSourceURL = 'https://cdn.flashtalking.com/feeds/test_data/att_product_data.json';
+    $fileBaseName = basename($fileSourceURL);
+    $fileContents = file_get_contents($fileSourceURL);
+    $fileForDisplay = json_decode($fileContents,true);
+
+    $header = false;
 
 ?>
 
@@ -14,6 +20,29 @@
         <link rel="stylesheet" href="">
     </head>
     <body>
+        <div>
+        <a href="https://cdn.flashtalking.com/feeds/test_data/att_product_data.json">File here</a>
+        </div>
+        <div>
+            <?php
+            foreach($fileForDisplay as $key => $value){
+                if(!is_array($value)){
+                    echo '<br>object index: ' . $key . '<br>';
+                } else {
+                    foreach($value as $sku => $skuEntry) {
+                        if (!is_array($skuEntry)){//this entry is always an array, so this should never run
+                            echo 'You Should not be here';
+                        } else {
+                            foreach($skuEntry as $singleSku => $skuEntryColumnHeading){//OKAY. so singleSku is the column heading ID. skuEntryColumnHeading is the data in each line, and some of these are still arrays
+                                echo '<br> column index: ' . $singleSku;
+                            }
+                        }
+                      }
+                }  
+            }
+             
 
+            ?>
+        </div>
     </body>
 </html>
